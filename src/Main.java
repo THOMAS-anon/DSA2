572b559a833a5181f54f8d2a710e7199357a0ebf
THOMAS-anon <85394885+THOMAS-anon@users.noreply.github.com>
2023-04-17 14:11:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList list= new LinkedList();
+        list.insert(5);
+        list.insert(18);
+        list.insert(12);
+        list.insertstart(24);
+        list.insertat(1,45);
+        list.show();
+    }
+}
